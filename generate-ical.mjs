af41c16857c3f80bec490a06ebd869635ca63bb1
Anna Fedyna <84384119+annafedyna@users.noreply.github.com>
2025-02-14 15:48:43 +0000

    Update generate-ical.mjs

--- generate-ical.mjs
+++ generate-ical.mjs
@@ -4,10 +4,6 @@ import {getCommemorativeDays} from './src/getCommemorativeDays.js'
 
 const START_YEAR = 2020;
 const END_YEAR = 2030;
-const MONTHS = {
-  January: 0, February: 1, March: 2, April: 3, May: 4, June: 5,
-  July: 6, August: 7, September: 8, October: 9, November: 10, December: 11
-};
 
 // Format date to iCal format (YYYYMMDDTHHmmssZ)
 function formatDateToICal(date) {
